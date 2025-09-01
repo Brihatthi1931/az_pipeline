43a569ad696bea847f9f21099a11fbef7f8533f5
Brihatthi1931 <brihatthi19@gmail.com>
2025-09-01 18:23:15 +0530

    added backend

--- parent/provider.tf
+++ parent/provider.tf
@@ -5,10 +5,15 @@ terraform {
       version = "4.42.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "abcrg"
+    storage_account_name = "abcstracc"
+    container_name = "abcon"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
     features {}
     subscription_id = "5e4fafbb-ef69-4e17-8835-68030d81f758"
-  # Configuration options
 }
